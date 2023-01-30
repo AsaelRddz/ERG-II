049f45bbd93b985e48017fe6166f1bad5f3849a5
Asael Rodriguez <ricardo.rodriguezl@milenio.com>
2023-01-29 23:24:24 -0600

    Initial commit

---
+++ app/src/main/java/com/erg_iiapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.erg_iiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

3ddab820d7a1f6d8b0a98a5dca26857df5932d30
Asael Rodriguez <ricardo.rodriguezl@milenio.com>
2023-01-31 05:31:30 -0600

    Creacion de vista login

---
+++ app/src/main/java/com/erg_iiapp/view/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.erg_iiapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

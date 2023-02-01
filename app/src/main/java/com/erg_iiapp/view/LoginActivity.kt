262374c6ff7b8ead8ffd634cb09dc7dc9af1ffca
Asael Rodriguez <ricardo.rodriguezl@milenio.com>
2023-02-01 00:17:24 -0600

    Avance login

--- app/src/main/java/com/erg_iiapp/view/LoginActivity.kt
+++ app/src/main/java/com/erg_iiapp/view/LoginActivity.kt
@@ -2,9 +2,12 @@ package com.erg_iiapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.erg_iiapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
     }
 }

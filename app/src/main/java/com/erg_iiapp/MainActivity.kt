9dda8960dced5c477c97ef8fc3b76ca6f8363e48
Asa <ricardo.rodriguezl@milenio.com>
2023-03-14 09:17:07 -0600

    prueba mac

--- app/src/main/java/com/erg_iiapp/MainActivity.kt
+++ app/src/main/java/com/erg_iiapp/MainActivity.kt
@@ -18,6 +18,8 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    //prueba
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)

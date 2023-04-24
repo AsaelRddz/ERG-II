52e7db8596f6d50c502ffd65e70e0e1fca213b11
Asael Rodriguez <asaelrddz00@gmail.com>
2023-04-23 23:58:05 -0600

    Avance rediseño interior ejercicio

--- app/src/main/java/com/erg_iiapp/view/ejercicios/BrazoActivity.kt
+++ app/src/main/java/com/erg_iiapp/view/ejercicios/BrazoActivity.kt
@@ -3,6 +3,7 @@ package com.erg_iiapp.view.ejercicios
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.erg_iiapp.R
 import com.erg_iiapp.databinding.ActivityBrazoBinding
 import com.erg_iiapp.model.DatosBrazos
@@ -43,14 +44,41 @@ class BrazoActivity : AppCompatActivity() {
             // Agregar OnClickListener a cada hijo
             child.setOnClickListener {
                 val position = binding.containerCards.indexOfChild(it) // obtiene la posición del hijo clickeado
-                val p = DatosBrazos()
-                p.uid = UUID.randomUUID().toString()
-                p.datosBrazos = position+1
 
-                // Crear un nodo con los datos en firebase
-                databaseReference!!.child("datosBrazos").child(p.uid!!).setValue(p)
-                startActivity(Intent(Intent(this, CalculoActivity::class.java)))
+                when(position){
+                    1 -> {
+                        binding.txtDescripcion1.visibility = View.VISIBLE
+                        binding.txtDescripcion2.visibility = View.GONE
+                        binding.txtDescripcion3.visibility = View.GONE
+                    }
+                    2 -> {
+                        binding.txtDescripcion1.visibility = View.GONE
+                        binding.txtDescripcion2.visibility = View.VISIBLE
+                        binding.txtDescripcion3.visibility = View.GONE
+                    }
+                    3 -> {
+                        binding.txtDescripcion1.visibility = View.GONE
+                        binding.txtDescripcion2.visibility = View.GONE
+                        binding.txtDescripcion3.visibility = View.VISIBLE
+                    }
+                }
+
+
             }
         }
+
+        binding.btnAddBrazo1.setOnClickListener { add(1) }
+        binding.btnAddBrazo2.setOnClickListener { add(2) }
+        binding.btnAddBrazo3.setOnClickListener { add(3) }
+    }
+
+    private fun add(position: Int){
+        val p = DatosBrazos()
+        p.uid = UUID.randomUUID().toString()
+        p.datosBrazos = position
+
+        // Crear un nodo con los datos en firebase
+        databaseReference!!.child("datosBrazos").child(p.uid!!).setValue(p)
+        startActivity(Intent(Intent(this, CalculoActivity::class.java)))
     }
 }
